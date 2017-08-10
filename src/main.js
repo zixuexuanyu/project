7f7f9aae97f297fec043c709d919f20395a67367
zixuexuanyu <2983068126@qq.com>
2017-08-10 16:53:27 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Layout from './components/layout'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import IndexPage from './pages/index'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    }
+  ]
+})
+
+new Vue({
+  el: '#app',
+  router,
+  template: '<Layout/>',
+  components: { Layout }
+})
